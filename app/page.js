9077c55d8f586cafaf149a9e7397c68419470db3
faroukahmed <thiagoacookie@gmail.com>
2024-02-26 15:47:07 +0300

    netlify forms

--- app/page.js
+++ app/page.js
@@ -1,24 +1,67 @@
-export default function Home() {
+'use client'
+import { useState, useEffect } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Register() {
+  const [registered, setRegistered] = useState(false);
+
+  useEffect(() => {
+    const isRegistered = localStorage.getItem('registered');
+    if (isRegistered === 'true') {
+      setRegistered(true);
+    }
+  }, []);
+
+  const handleFormSubmit = () => {
+    setRegistered(true);
+    localStorage.setItem('registered', 'true');
+  };
+
+  if (registered) {
+    return (
+        <div id='Register-success'>
+          <div className='success-register-container'>
+            <h1 className='success-header'>Success</h1>
+            <p className='success-p-tag'>Your account has been created</p>
+            <button className="btn-success button-success btn-sm" type="button">
+              <Link className="nav-link" href='#'>CONTINUE</Link>
+            </button>
+          </div>
+        </div>
+    );
+  }
+
   return (
-   <div>
-     <form name="contact" data-netlify="true" method='post'>
-       <input type='hidden' name='form-name' value='contact'/>
-       <div className="mb-lg-4 mb-md-3 mb-2">
-         <label htmlFor="your-name" className="form-label">Your Name</label>
-         <input type="text" className="form-control" id="your-name" name='your-name' required/>
-       </div>
-       <div className="mb-lg-4 mb-md-3 mb-2">
-         <label htmlFor="email" className="form-label">Your Email</label>
-         <input type="email" className="form-control" id="email" name='user-email' required/>
-       </div>
-       <div className='mb-lg-4 mb-md-3 mb-2'>
-         <label htmlFor="phone-number" className="form-label">Number</label>
-         <input type="tel" className="form-control" id="phone-number" required/>
-       </div>
-       <div className='d-flex justify-content-center'>
-         <input type="submit" className='btn sign-up-btn' value='Sign Up'/>
-       </div>
-     </form>
-   </div>
+      <div id='Register'>
+        <div className='sign-up-container'>
+          <div className='d-flex align-items-center flex-column mb-lg-4 mb-md-3'>
+            <h1 className='sign-up-header'>Create an Account</h1>
+            <Image src='/images/logo-dawah-academy.svg' alt='logo-dawah-academy' className='mb-2' height='100' width='100'/>
+          </div>
+          <form name="contact v1" data-netlify="true" method='post' onSubmit={handleFormSubmit} data-netlify-honeypot='bot-field'>
+            <input type='hidden' name='form-name' value='contact v1'/>
+            <div hidden>
+              <input name='bot-field'/>
+            </div>
+            <div className="mb-lg-4 mb-md-3 mb-2">
+              <label htmlFor="your-name" className="form-label">Your Name</label>
+              <input type="text" className="form-control" id="your-name" name='your-name' required/>
+            </div>
+            <div className="mb-lg-4 mb-md-3 mb-2">
+              <label htmlFor="email" className="form-label">Your Email</label>
+              <input type="email" className="form-control" id="email" name='user-email' required/>
+            </div>
+            <div className='mb-lg-4 mb-md-3 mb-2'>
+              <label htmlFor="phone-number" className="form-label">Number</label>
+              <input type="tel" className="form-control" id="phone-number" required/>
+            </div>
+            <div className='d-flex justify-content-center'>
+              <input type="submit" className='btn sign-up-btn' value='Sign Up'/>
+            </div>
+          </form>
+        </div>
+      </div>
   );
 }
+
