46fb76bf6b2a883dc1a51e3f3ed8aef20ac99d9d
faroukahmed <thiagoacookie@gmail.com>
2024-03-06 08:45:25 +0300

    Sending administration form data to netlify

--- app/test/page.js
+++ app/test/page.js
@@ -6,6 +6,15 @@ const ParentInfo = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        try {
+            await fetch('/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams({ 'phone-number': phoneNumber }).toString(),
+            });
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
     };
 
     const handleChange = (e) => {
@@ -15,13 +24,13 @@ const ParentInfo = () => {
     return (
         <div>
             <h1 style={{ textAlign: 'center', fontWeight: 'bold', margin: '30px 0', fontSize: '2rem' }}>Parent Information</h1>
-            <form name="test" data-netlify="true" method='post' onSubmit={handleSubmit} data-netlify-honeypot='bot-field' action="/success" data-netlify-success="/success">
+            <form name="test" data-netlify="true" method='post' onSubmit={handleSubmit} data-netlify-honeypot='bot-field' action="">
                 <div className='mb-lg-4 mb-md-3 mb-2'>
                     <label htmlFor="phone-number" className="form-label">Test</label>
-                    <input type="tel" className="form-control" id="phone-number" name="phone-number" value={phoneNumber} onChange={handleChange} required/>
+                    <input type="tel" className="form-control" id="phone-number" name="phone-number" value={phoneNumber} onChange={handleChange} required />
                 </div>
                 <div className='d-flex justify-content-center'>
-                    <input type="submit" className='btn sign-up-btn' value='Sign Up'/>
+                    <input type="submit" className='btn sign-up-btn' value='Sign Up' />
                 </div>
             </form>
         </div>
