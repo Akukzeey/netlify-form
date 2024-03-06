b2d1ded3adf90b82abe1840ca80e84eb3d19af2a
faroukahmed <thiagoacookie@gmail.com>
2024-03-06 08:59:00 +0300

    Sending administration form data to netlify

--- app/test/page.js
+++ app/test/page.js
@@ -25,26 +25,14 @@ export default function RegisterComponent() {
                 <div className='d-flex align-items-center flex-column mb-lg-4 mb-md-3'>
                     <h1 className='sign-up-header'>Create an Account</h1>
                 </div>
-                <form name="contact v3" data-netlify="true" method='post' onSubmit={handleSubmit} data-netlify-honeypot='bot-field' action="/success" data-netlify-success="/success">
-                    <input type='hidden' name='form-name' value='contact v3'/>
+                <form name="test" data-netlify="true" method='post' onSubmit={handleSubmit} data-netlify-honeypot='bot-field' action="/success" data-netlify-success="/success">
+                    <input type='hidden' name='form-name' value='test'/>
                     <div hidden>
                         <input name='bot-field'/>
                     </div>
                     <div className="mb-lg-4 mb-md-3 mb-2">
                         <label htmlFor="your-name" className="form-label">Your Name</label>
                         <input type="text" className="form-control" id="your-name" name='your-name' required/>
-                    </div>
-                    <div className="mb-lg-4 mb-md-3 mb-2">
-                        <label htmlFor="email" className="form-label">Your Email</label>
-                        <input type="email" className="form-control" id="email" name='user-email' value={email} onChange={handleEmailChange} required/>
-                        {emailExists && <p style={{ color: 'red' }}>This email already exists. Please use a different email.</p>}
-                    </div>
-                    <div className='mb-lg-4 mb-md-3 mb-2'>
-                        <label htmlFor="phone-number" className="form-label">Number</label>
-                        <input type="tel" className="form-control" id="phone-number" name="phone-number" required/>
-                    </div>
-                    <div className='d-flex justify-content-center'>
-                        <input type="submit" className='btn sign-up-btn' value='Sign Up'/>
                     </div>
                 </form>
             </div>
